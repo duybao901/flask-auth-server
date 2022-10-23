fb68b6a06871ecf3dba871370fa25231b2daba69
duycaryy123 <duycarry123@gmail.com>
2022-10-23 16:19:00 +0700

    test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ config.read(os.path.abspath(os.path.join("config.ini")))
 
 if __name__ == "__main__":
   app = create_app()
-  app.config['MONGO_URI'] = config['PROD']['DB_URI']  
+  app.config['MONGO_URI'] = "mongodb+srv://baoduy123:baoduy123@cluster0.ubjer5c.mongodb.net/face-data?retryWrites=true&w=majority"
  
   app.run(debug=True)      
 
