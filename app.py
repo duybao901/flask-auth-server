cfdc66b48dfc74ddb8a081eaba0b176e320b9aca
duycaryy123 <duycarry123@gmail.com>
2022-10-23 16:41:18 +0700

    test

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
-# import os
-# import configparser
-# from App.factory import create_app
+import os
+import configparser
+from App.factory import create_app
 
-# config = configparser.ConfigParser()
-# config.read(os.path.abspath(os.path.join("config.ini")))
+config = configparser.ConfigParser()
+config.read(os.path.abspath(os.path.join("config.ini")))
 
-# if __name__ == "__main__":
-#   app = create_app()
-#   app.config['MONGO_URI'] = "mongodb+srv://baoduy123:baoduy123@cluster0.ubjer5c.mongodb.net/face-data?retryWrites=true&w=majority"
+if __name__ == "__main__":
+  app = create_app()
+  app.config['MONGO_URI'] = "mongodb+srv://baoduy123:baoduy123@cluster0.ubjer5c.mongodb.net/face-data?retryWrites=true&w=majority"
  
-#   app.run(debug=True)      
+  app.run(debug=True)      
 
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-  return "Hello world"
   
   
